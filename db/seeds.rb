c80d769d15b0666309806072fa0bf1e84d3273a4
Grace Bunardi <gbunardi@hotmail.com>
2022-08-30 16:07:26 +0800

    updated contact field from job to title

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#accounts
+grab = Account.new(name: "Grab", category: "transportation", status: "Lead", potential_rev:1000000)
+gojek = Account.new(name: "Gojek", category: "transportation", status: "Prospect", potential_rev:500000)
+apple = Account.new(name: "Apple", category: "consumer electronics", status: "Live", potential_rev:1500000)
+samsung = Account.new(name: "Samsung", category: "consumer electronics", status: "Churned", potential_rev:1250000)
+
+#contacts
