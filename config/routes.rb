576b76f4c5af58603ba28d73490bf5b575178b30
Sarina Ke <sarina.y.ke@gmail.com>
2022-08-30 15:19:21 +0800

    finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
+  resources :pages, only: [:index]
 
+  resources :accounts, only: [:index, :show]
+
+  resources :templates, only: [:index, :create, :update, :delete]
+
+  resources :triggers, only: [:index, :create, :update, :delete]
+
+  resources :drafts, only: [:index, :update, :delete]
 end
