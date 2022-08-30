81d9360f7042edb2c64103414d14df413b206df2
Sarina Ke <sarina.y.ke@gmail.com>
2022-08-30 15:26:50 +0800

    created controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "pages#home"
-  resources :pages, only: [:index]
+  root to: "pages#home" # landing page
+  resources :pages, only: %i[index] # welcome CRM page
 
-  resources :accounts, only: [:index, :show]
+  resources :accounts, only: %i[index show]
 
-  resources :templates, only: [:index, :create, :update, :delete]
+  resources :templates, only: %i[index create update delete]
 
-  resources :triggers, only: [:index, :create, :update, :delete]
+  resources :triggers, only: %i[index create update delete]
 
-  resources :drafts, only: [:index, :update, :delete]
+  resources :drafts, only: %i[index update delete]
 end
