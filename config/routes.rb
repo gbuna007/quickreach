ac15c65de7cfc0bcd7e7b4df380d39d854b785be
Sarina Ke <sarina.y.ke@gmail.com>
2022-08-31 13:04:21 +0800

    edited landing, CRM, and dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "pages#home" # landing page
-  resources :pages, only: %i[index] # welcome CRM page
+  # root to: "pages#home" # landing page
+  # resources :pages, only: %i[index] # welcome CRM page
+  root to: "pages#index"
 
   resources :accounts, only: %i[index show]
 
