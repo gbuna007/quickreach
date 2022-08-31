6eb7e55a0198343000c31f7c3a0ff64e7d1abc84
Grace Bunardi <gbunardi@hotmail.com>
2022-08-31 13:38:25 +0800

    added in some sample json and added in a route for news api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  namespace :api do
+    namespace :v1 do
+      get '/news/:client', to: 'news#fetch'
+    end
+  end
+
   root to: "pages#home" # landing page
   resources :pages, only: %i[index] # welcome CRM page
 
