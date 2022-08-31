212848b021d66397b90de73ec3679a1f2adde359
Grace Bunardi <gbunardi@hotmail.com>
2022-08-31 16:35:10 +0800

    merge routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-<<<<<<< HEAD
   namespace :api do
     namespace :v1, defaults: { format: 'json' } do
       get '/news', to: 'news#index'
     end
   end
 
-  # root to: "pages#home" # landing page
-  # resources :pages, only: %i[index] # welcome CRM page
-  root to: "pages#index"
-=======
   root to: "pages#home" # landing page
   resources :pages, only: %i[index] # welcome CRM page
->>>>>>> main
 
   resources :accounts, only: %i[index show]
 
