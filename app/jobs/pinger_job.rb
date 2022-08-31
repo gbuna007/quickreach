ea6bc7c0b320ce5b116183756ee0d2fca0645727
Grace Bunardi <gbunardi@hotmail.com>
2022-08-31 18:25:28 +0800

    conditionals to link MatcherJob and PingerJob

--- app/jobs/pinger_job.rb
+++ app/jobs/pinger_job.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 require 'json'
-require 'pry-byebug'
+# require 'pry-byebug'
 
 class PingerJob < ApplicationJob
   queue_as :default
@@ -9,5 +9,11 @@ class PingerJob < ApplicationJob
     url = "http://localhost:3000/api/v1/news"
     json = URI.open(url).read
     data = JSON.parse(json)
+
+    if data.count > 0
+      data.each do |news|
+        MatcherJob.perform_later(news.id)
+      end
+    end
   end
 end
