5ddb9a9fb72b72b40c769fd033375d1b6dcf50cf
Grace Bunardi <gbunardi@hotmail.com>
2022-09-01 15:39:15 +0800

    init Draft policy

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -1,2 +1,12 @@
 class DraftsController < ApplicationController
+  before_action :set_draft, only: %i[edit show]
+
+  def edit
+  end
+
+  private
+
+  def set_draft
+    @draft = Draft.find(params[:id])
+  end
 end
--- db/migrate/20220901061659_create_drafts.rb
+++ db/migrate/20220901061659_create_drafts.rb
@@ -3,7 +3,7 @@ class CreateDrafts < ActiveRecord::Migration[7.0]
     create_table :drafts do |t|
       t.references :trigger, null: false, foreign_key: true
       t.references :template, null: false, foreign_key: true
-      t.boolean :sent
+      t.boolean :sent, default: false
 
       t.timestamps
     end
