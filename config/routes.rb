00fe6b84b73d35a8125a95dca3136c0167a23e2a
Grace Bunardi <gbunardi@hotmail.com>
2022-09-02 09:37:52 +0800

    routes set, view copied

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   # if a user signs up, take them to the CRM page
   # if a user logs in to an existing account, take them to account dashboard
-  devise_for :users, controllers: { registrations: 'users/registrations' }, only: %i[new after_sign_up_path_for after_sign_in_path_for]
+  devise_for :users, controllers: { registrations: 'users/registrations' }, only: %i[new]
 
   root to: "pages#home" # landing page
   resources :pages, only: %i[index] # CRM page
