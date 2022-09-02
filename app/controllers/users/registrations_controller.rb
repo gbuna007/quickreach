bb83763a9cff7e32c15fcfe56ccee805f613884f
Grace Bunardi <gbunardi@hotmail.com>
2022-09-02 10:00:37 +0800

    post sign up redirection to crm page works

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,19 +4,6 @@ class Users::RegistrationsController < Devise::SessionsController
   # end
 
   def after_sign_up_path_for(resource)
-    raise
-    if current_user.accounts.count.positive?
-      # if user already integrated their CRM
-      accounts_path(resource)
-    else
-      # if user hasn't integrated
-      pages_path(resource)
-    end
-
-    # after_sign_in_path_for(resource) if is_navigational_format?
-  end
-
-  def after_sign_in_path_for(resource)
-    raise
+    pages_path(resource)
   end
 end
