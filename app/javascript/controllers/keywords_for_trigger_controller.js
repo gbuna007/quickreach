a4013b68e3119cd30c9d021e141677798fa09b30
Alfred Tay <twx.alfred@gmail.com>
2022-09-03 14:47:37 +0800

    cleead up files, preparing merge with master

--- app/javascript/controllers/keywords_for_trigger_controller.js
+++ app/javascript/controllers/keywords_for_trigger_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="keywords-for-trigger"
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["form", "keyItems"]
   connect() {
     console.log("Hello from keywords kontoller")
   }
