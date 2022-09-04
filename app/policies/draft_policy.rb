dd2e15a384220caa7aaf62d8568932d9d58d51cc
Alfred Tay <twx.alfred@gmail.com>
2022-09-04 18:20:09 +0800

    Drafts page done, need to find users, TODO: implement method that find users

--- app/policies/draft_policy.rb
+++ app/policies/draft_policy.rb
@@ -1,12 +1,16 @@
 class DraftPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.where(user: user)
+    end
   end
 
-  def edit?
+  def index
+    record.empty? ? true : record.first.user == user
+  end
+
+  def update?
     record.template.user == user
   end
 end
