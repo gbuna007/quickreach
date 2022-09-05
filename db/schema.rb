fe7ff787397784c83681564eea54be75815085d9
Sarina Ke <sarina.y.ke@gmail.com>
2022-09-05 12:36:52 +0800

    did drafts page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_093915) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_061659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,18 +71,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_093915) do
     t.string "full_description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "notifications", force: :cascade do |t|
-    t.string "recipient_type", null: false
-    t.bigint "recipient_id", null: false
-    t.string "type", null: false
-    t.jsonb "params"
-    t.datetime "read_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["read_at"], name: "index_notifications_on_read_at"
-    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
   create_table "old_news", force: :cascade do |t|
