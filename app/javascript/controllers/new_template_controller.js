62a299faa67155d7f799d55601b5fe00d1c2b280
Sarina Ke <sarina.y.ke@gmail.com>
2022-09-06 13:45:48 +0800

    updated css on template page

--- app/javascript/controllers/new_template_controller.js
+++ app/javascript/controllers/new_template_controller.js
@@ -27,11 +27,15 @@ export default class extends Controller {
     })
       .then(response => response.json())
       .then((data) => { // data is what is returned by create.json.jbuilder
-        if (data.inserted_item) { // if template is saved (form is submitted properly)
-          this.rowsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
-        }
         // replace the old form with a new one / the old one with errors
         this.submitTarget.outerHTML = data.form
+
+        if (data.inserted_item) { // if template is saved (form is submitted properly)
+          this.rowsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
+        } else {
+          // means there are errors
+          this.formTarget.classList.remove("d-none")
+        }
       })
   }
 }
