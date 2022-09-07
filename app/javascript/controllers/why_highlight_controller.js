314ec5ca24f3f60a35ebd683e3deb6873d55f088
Grace Bunardi <gbunardi@hotmail.com>
2022-09-07 13:56:12 +0800

    added animation for images

--- app/javascript/controllers/why_highlight_controller.js
+++ app/javascript/controllers/why_highlight_controller.js
@@ -12,32 +12,32 @@ export default class extends Controller {
     const id = e.currentTarget.dataset.id;
     switch(id) {
       case "1":
-        const desc1 = "<h3>Accurate data through CRM Integration</h3><p>Quickly reach out to relevant accounts and contacts through your CRM as source-of-truth.</p>\
+        const desc1 = "<p>Quickly reach out to relevant accounts and contacts through your CRM as source-of-truth.</p>\
         <p>As the data is synced with your CRM, any updates within CRM will be reflected on QuickReach.</p>"
         this.descriptionTarget.innerHTML = desc1;
         e.currentTarget.classList.add("active")
-        this.imageTarget.innerHTML = "<img width='300' height='250' class='highlight-image-why' src='http://res.cloudinary.com/twx-a/image/upload/c_fill,h_250,w_300/development/integrate_crm.png'>"
+        this.imageTarget.innerHTML = "<img width='300' height='250' class='highlight-image-why' src='http://res.cloudinary.com/twx-a/image/upload/c_fill,h_225,w_275/development/integrate_crm.png'>"
         break;
       case "2":
-        const desc2 = "<h3>Automate your workflow</h3><p>Process of reading breaking news, deciding their relevancy, and drafting emails is repetitive and takes a lot of time.</p>\
+        const desc2 = "<p>Process of reading breaking news, deciding their relevancy, and drafting emails is repetitive and takes a lot of time.</p>\
         <p>Through automation QuickReach will enable you to focus on high-value activities such as speaking to your clients and prospects.</p>"
         this.descriptionTarget.innerHTML = desc2;
         e.currentTarget.classList.add("active")
-        this.imageTarget.innerHTML = "<img width='300' height='250' class='highlight-image-why' src='http://res.cloudinary.com/twx-a/image/upload/c_fill,h_250,w_300/development/automate_news.png'>"
+        this.imageTarget.innerHTML = "<img width='300' height='250' class='highlight-image-why' src='http://res.cloudinary.com/twx-a/image/upload/c_fill,h_225,w_275/development/automate_news.png'>"
         break;
       case "3":
-        const desc3 = "<h3>Automated news query</h3><p>Delegate news research to us.</p>\
+        const desc3 = "<p>Delegate news research to us.</p>\
         <p>Our system is connected to top global and regional publications and will check for any breaking news every 5 minutes.</p>"
         this.descriptionTarget.innerHTML = desc3;
         e.currentTarget.classList.add("active")
-        this.imageTarget.innerHTML = "<img width='300' height='250' class='highlight-image-why' src='http://res.cloudinary.com/twx-a/image/upload/c_fill,h_250,w_300/development/newspaper.png'>"
+        this.imageTarget.innerHTML = "<img width='300' height='250' class='highlight-image-why' src='http://res.cloudinary.com/twx-a/image/upload/c_fill,h_225,w_275/development/newspaper.png'>"
         break;
       case "4":
-        const desc4 = "<h3>Pre-created drafts ready for you to send</h3><p>When there is a relevant breaking news, QuickReach will generate a draft and notify you</p>\
+        const desc4 = "<p>When there is a relevant breaking news, QuickReach will generate a draft and notify you</p>\
         <p>Drafts will be pre-polulated with the right information and are ready to send after a review.</p>"
         this.descriptionTarget.innerHTML = desc4;
         e.currentTarget.classList.add("active")
-        this.imageTarget.innerHTML = "<img width='300' height='250' class='highlight-image-why' src='http://res.cloudinary.com/twx-a/image/upload/c_fill,h_250,w_300/development/email.png'>"
+        this.imageTarget.innerHTML = "<img width='300' height='250' class='highlight-image-why' src='http://res.cloudinary.com/twx-a/image/upload/c_fill,h_225,w_275/development/email.png'>"
         break;
     }
   }
