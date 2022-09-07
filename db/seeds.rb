7e700f4f0a36771ea4c90510e0d455f66f105ec9
Grace Bunardi <gbunardi@hotmail.com>
2022-09-07 20:00:32 +0800

    added seed for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ user1 = User.create(first_name: "Ann", last_name: "Lim", email: "ann@gmail.com",
 user2 = User.create(first_name: "Bob", last_name: "Lee", email: "bob@gmail.com", password: "bob123") # have accounts, contacts, NO triggers
 user3 = User.create(first_name: "Chan", last_name: "Chan", email: "chan@gmail.com", password: "chan123") # no accounts
 admin = User.create(email: "seb@lewagon.org", password: "password", admin: true)
+user4 = User.create(first_name: "Grace", last_name: "Bunardi", email: "grace@random-forest.io", password: "grace123")
 
 # Ann's accounts
 grab = Account.new(name: "Grab", category: "transportation", status: "Lead", potential_rev: 1000000, amount_spent: 1000)
