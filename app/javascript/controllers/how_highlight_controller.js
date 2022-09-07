ab8e41d75b7ca1d49adae2dc330335a834e9e75a
Grace Bunardi <gbunardi@hotmail.com>
2022-09-07 11:22:07 +0800

    added in changing highlighed card - how+why

--- app/javascript/controllers/how_highlight_controller.js
+++ app/javascript/controllers/how_highlight_controller.js
@@ -2,38 +2,38 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="how-highlight"
 export default class extends Controller {
-  static targets = ["description"]
+  static targets = ["description", "card"]
 
   connect() {
     console.log("how highlight connected");
   }
 
   update(e) {
-    console.log(e.currentTarget.dataset.id);
+    this.cardTargets.forEach(ele => ele.classList.remove("active"))
     const id = e.currentTarget.dataset.id;
     switch(id) {
       case "1":
         const desc1 = "<p>We have existing integration available with world's major CRMs: Salesforce, Hubspot, Zoho, and Sugar CRM.</p>"
-        console.log(desc1);
         this.descriptionTarget.innerHTML = desc1;
+        e.currentTarget.classList.add("active")
         break;
       case "2":
         const desc2 = "<p>aaaaaaaaa.</p>\
         <p>aaaaaaa.</p>"
-        console.log(desc2);
         this.descriptionTarget.innerHTML = desc2;
+        e.currentTarget.classList.add("active")
         break;
       case "3":
         const desc3 = "<p>bbbbbbb.</p>\
         <p>bbbbbbbb.</p>"
-        console.log(desc3);
         this.descriptionTarget.innerHTML = desc3;
+        e.currentTarget.classList.add("active")
         break;
       case "4":
         const desc4 = "<p>ccccccc.</p>\
         <p>ccccccc.</p>"
-        console.log(desc4);
         this.descriptionTarget.innerHTML = desc4;
+        e.currentTarget.classList.add("active")
         break;
     }
   }
--- app/javascript/controllers/why_highlight_controller.js
+++ app/javascript/controllers/why_highlight_controller.js
@@ -5,40 +5,33 @@ export default class extends Controller {
   static targets = ["description", "card"]
 
   connect() {
-    console.log("why highlight connected");
-    console.log(this.cardTargets);
   }
 
   update(e) {
-    console.log(e.currentTarget.dataset.id);
     this.cardTargets.forEach(ele => ele.classList.remove("active"))
     const id = e.currentTarget.dataset.id;
     switch(id) {
       case "1":
         const desc1 = "<p>Quickly reach out to relevant accounts and contacts throough your CRM as source-of-truth.</p>\
         <p>As the data is synced with your CRM, any updates within CRM will be reflected on QuickReach.</p>"
-        console.log(desc1);
         this.descriptionTarget.innerHTML = desc1;
         e.currentTarget.classList.add("active")
         break;
       case "2":
         const desc2 = "<p>Process of reading breaking news, deciding their relevancy, and drafting emails is repetitive and takes a lot of time.</p>\
         <p>Through automation QuickReach will enable you to focus on high-value activities such as speaking to your clients and prospects.</p>"
-        console.log(desc2);
         this.descriptionTarget.innerHTML = desc2;
         e.currentTarget.classList.add("active")
         break;
       case "3":
         const desc3 = "<p>yyyyy.</p>\
         <p>yyyyy.</p>"
-        console.log(desc3);
         this.descriptionTarget.innerHTML = desc3;
         e.currentTarget.classList.add("active")
         break;
       case "4":
         const desc4 = "<p>zzzzzz.</p>\
         <p>zzzz.</p>"
-        console.log(desc4);
         this.descriptionTarget.innerHTML = desc4;
         e.currentTarget.classList.add("active")
         break;
