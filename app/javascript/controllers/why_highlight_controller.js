433e7aff29ac4b02b9eded051e52f869c3c14697
Grace Bunardi <gbunardi@hotmail.com>
2022-09-07 11:19:17 +0800

    added in changing highlighed card

--- app/javascript/controllers/why_highlight_controller.js
+++ app/javascript/controllers/why_highlight_controller.js
@@ -2,15 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="why-highlight"
 export default class extends Controller {
-  static targets = ["description"]
+  static targets = ["description", "card"]
 
   connect() {
     console.log("why highlight connected");
-    // console.log(this.descriptionTarget.innerHTML);
+    console.log(this.cardTargets);
   }
 
   update(e) {
     console.log(e.currentTarget.dataset.id);
+    this.cardTargets.forEach(ele => ele.classList.remove("active"))
     const id = e.currentTarget.dataset.id;
     switch(id) {
       case "1":
@@ -18,24 +19,28 @@ export default class extends Controller {
         <p>As the data is synced with your CRM, any updates within CRM will be reflected on QuickReach.</p>"
         console.log(desc1);
         this.descriptionTarget.innerHTML = desc1;
+        e.currentTarget.classList.add("active")
         break;
       case "2":
         const desc2 = "<p>Process of reading breaking news, deciding their relevancy, and drafting emails is repetitive and takes a lot of time.</p>\
         <p>Through automation QuickReach will enable you to focus on high-value activities such as speaking to your clients and prospects.</p>"
         console.log(desc2);
         this.descriptionTarget.innerHTML = desc2;
+        e.currentTarget.classList.add("active")
         break;
       case "3":
         const desc3 = "<p>yyyyy.</p>\
         <p>yyyyy.</p>"
         console.log(desc3);
         this.descriptionTarget.innerHTML = desc3;
+        e.currentTarget.classList.add("active")
         break;
       case "4":
         const desc4 = "<p>zzzzzz.</p>\
         <p>zzzz.</p>"
         console.log(desc4);
         this.descriptionTarget.innerHTML = desc4;
+        e.currentTarget.classList.add("active")
         break;
     }
   }
